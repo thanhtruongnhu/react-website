88e89f7e89e71fbdb42f60265fd4d3d2c2cd1218
truong.nhu <35893692+thanhtruongnhu@users.noreply.github.com>
2021-01-16 13:57:05 -0400

    service-product-signup pages creation

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -17,8 +17,8 @@ function Cards() {
                             path='/service'
                         />
                         <CardItem
-                            src="/images/img-9.jpg" 
-                            text="Explore the deep insided the Amazon Jungle"
+                            src="/images/img-2.jpg" 
+                            text="Travel through the Islands of Bali in a Private Cruise"
                             label='Luxury'
                             path='/service'
                         />                                              
@@ -26,17 +26,23 @@ function Cards() {
                     <ul className="cards__items">
                         {/*Gonna put the <li> element created in CardItem.js here */}
                         <CardItem
-                            src="/images/img-9.jpg" 
-                            text="Explore the hidden waterfall deep insided the Amazon Jungle"
+                            src='images/img-3.jpg'
+                            text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
                             label='Adventure'
                             path='/service'
                         />
                         <CardItem
-                            src="/images/img-2.jpg" 
-                            text="Explore the hidden waterfall deep insided the Amazon Jungle"
+                            src='images/img-4.jpg'
+                            text='Experience Football on Top of the Himilayan Mountains'
                             label='Luxury'
                             path='/service'
-                        />                                              
+                        />            
+                        <CardItem
+                            src='images/img-8.jpg'
+                            text='Ride through the Sahara Desert on a guided camel tour'
+                            label='Luxury'
+                            path='/service'
+                        />                                                             
                     </ul>                    
                 </div>
             </div>
